25533d206c38d01dc531f1e9fd90062a6c4a07b6
dvpappas <dvpappas89@gmail.com>
2023-06-14 13:11:04 +0300

    initial commit

--- using_official_dumps/h2020/download_all.sh
+++ using_official_dumps/h2020/download_all.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
+cat url_list.txt | parallel -j8 wget {}
 
-wget https://cordis.europa.eu/data/cordis-h2020projectPublications-json.zip
-wget https://cordis.europa.eu/data/cordis-h2020projects-json.zip
-wget https://cordis.europa.eu/data/cordis-h2020reports-json.zip
-wget https://cordis.europa.eu/data/cordis-h2020projectDeliverables-json.zip
-wget https://cordis.europa.eu/data/cordis-h2020-erc-pi.xlsx
-wget https://cordis.europa.eu/data/projectIrps_h2020.xlsx
-wget https://cordis.europa.eu/data/reference/cordisref-H2020programmes-xlsx.zip
-wget https://cordis.europa.eu/data/reference/cordisref-h2020topicKeywords.xlsx
-wget https://cordis.europa.eu/data/reference/cordisref-H2020topics-xlsx.zip
-wget https://cordis.europa.eu/data/cordis-h2020projectPublications-xlsx.zip
-wget https://cordis.europa.eu/data/cordis-h2020projects-xlsx.zip
-wget https://cordis.europa.eu/data/cordis-h2020reports-xlsx.zip
-wget https://cordis.europa.eu/data/cordis-h2020projectDeliverables-xlsx.zip
 unzip cordis-h2020projectPublications-json.zip
 unzip cordis-h2020projects-json.zip
 unzip cordis-h2020reports-json.zip
