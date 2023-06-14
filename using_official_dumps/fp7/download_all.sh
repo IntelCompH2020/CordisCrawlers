e6e8418c770a6f5745b79a56b096056d629ea8f8
dvpappas <dvpappas89@gmail.com>
2023-06-14 13:13:52 +0300

    initial commit

--- using_official_dumps/fp7/download_all.sh
+++ using_official_dumps/fp7/download_all.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-wget https://cordis.europa.eu/data/cordis-fp7projects-json.zip
-wget https://cordis.europa.eu/data/cordis-fp7reports-json.zip
-wget https://cordis.europa.eu/data/FP7PC_DM_PROJ_PUBLICATIONS.xlsx
-wget https://cordis.europa.eu/data/projectIrps_fp7.xlsx
-wget https://cordis.europa.eu/data/reference/cordisref-FP7programmes.xls
-wget https://cordis.europa.eu/data/reference/cordisref-fp7subprogrammes.txt
-wget https://cordis.europa.eu/data/cordis-fp7projects-xlsx.zip
-wget https://cordis.europa.eu/data/cordis-fp7reports-xlsx.zip
+cat url_list.txt | parallel -j8 wget {}
+
 unzip cordis-fp7projects-json.zip
 unzip cordis-fp7reports-json.zip
 unzip cordis-fp7projects-xlsx.zip
